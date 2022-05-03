deaa892c8ce4fb0a4bb5bc3bf460234f87b4e97f
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-05-03 07:03:49 -0600

    Remove Console Log

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ function formatDate(timestamp) {
 
 
 function displayTemperature(response) {
-    console.log(response.data.main.temp)
+    
     let temperatureElement=document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round (response.data.main.temp);
 

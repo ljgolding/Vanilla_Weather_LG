c90acc0da73fae768385c0f2783b54071a654bd2
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-05-11 07:05:38 -0600

    add Forecast Loop

--- src/app.js
+++ src/app.js
@@ -13,11 +13,43 @@ function formatDate(timestamp) {
 
 
     return `${day} ${hours}:${minutes}`
-
-
-
-   
+  
 }
+
+function displayForecast(){
+  let forecastElement = document.querySelector("#forecast");
+  let days =["thu", "fri", "sat"];
+ 
+  let forecastHTML = `<div class = "row">`;
+  
+
+  days.forEach(function(day) {
+
+    forecastHTML = forecastHTML + 
+  
+  `
+            <div class="col-2">
+              <div class="weather-forecast-date">
+                ${day}
+                </div>
+              <img src=https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png alt="" width="36"/>
+               
+                <div class="weather-forecast-temperatures">
+              
+                  <span class="weather-forecast-temperature-max">18 </span> 
+                  <span class="weather-forecast-temperature-min">12</span>
+               </div>
+            </div>
+              
+  `;
+ });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+
+}
+
+
 
 
 function displayTemperature(response) {
@@ -86,9 +118,6 @@ function displayCelsiusTemperature(event) {
    temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-
-search ("Calgary")
-
 let celsiusTemperature = null;
 
 let form=document.querySelector("#search-form");
@@ -99,3 +128,7 @@ fahrenheitLink.addEventListener("click",displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
+search ("Calgary");
+displayForecast();

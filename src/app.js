37b6035d9c8751d52aa6c9abf1b17316e17c6961
ljgolding <101936562+ljgolding@users.noreply.github.com>
2022-05-11 07:43:01 -0600

    Display Forecast Data using API

--- src/app.js
+++ src/app.js
@@ -24,34 +24,54 @@ function getForecast(coordinates) {
   axios.get(apiURL).then(displayForecast);
 
 }
+
+
+function formatDay(timestamp){
+
+  let date = new Date (timestamp * 1000);
+  let day= date.getDay();
+  let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+  
+  return days[day];
+
+
+
+}
+
+
+
+
 function displayForecast(response){
 
-  console.log (response.data.daily);
+  let forecast = response.data.daily;
+
+
   let forecastElement = document.querySelector("#forecast");
   let days =["thu", "fri", "sat"];
  
   let forecastHTML = `<div class = "row">`;
   
 
-  days.forEach(function(day) {
-
+ forecast.forEach(function(forecastDay,index) {
+if (index < 6) {
     forecastHTML = forecastHTML + 
   
   `
             <div class="col-2">
               <div class="weather-forecast-date">
-                ${day}
+                ${formatDay(forecastDay.dt)}
                 </div>
-              <img src=https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png alt="" width="36"/>
+              <img src="http:/openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="36"/>
                
                 <div class="weather-forecast-temperatures">
               
-                  <span class="weather-forecast-temperature-max">18 </span> 
-                  <span class="weather-forecast-temperature-min">12</span>
+                  <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)} </span> 
+                  <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}</span>
                </div>
             </div>
               
   `;
+}
  });
 
   forecastHTML = forecastHTML + `</div>`;
